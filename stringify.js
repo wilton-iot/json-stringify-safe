e3dbb38db154f5c37e9b5cba41e4cfa6668718b4
alex <alex@staticlibs.net>
2017-12-01 00:21:22 +0000

    browser compat

--- stringify.js
+++ stringify.js
@@ -1,4 +1,4 @@
-define(function(){var require = WILTON_requiresync;var module = {exports: {}};var exports = module.exports;
+define(function(localRequire, exports, module) { var requireOrig = require; require = localRequire;
 exports = module.exports = stringify
 exports.getSerialize = serializer
 
@@ -27,4 +27,4 @@ function serializer(replacer, cycleReplacer) {
   }
 }
 
-return module.exports;});
+require = requireOrig;});
--- test/stringify_test.js
+++ test/stringify_test.js
@@ -1,4 +1,4 @@
-define(function(){var require = WILTON_requiresync;var module = {exports: {}};var exports = module.exports;
+define(function(localRequire, exports, module) { var requireOrig = require; require = localRequire;
 var Sinon = require("sinon")
 var stringify = require("..")
 function jsonify(obj) { return JSON.stringify(obj, null, 2) }
@@ -246,4 +246,4 @@ function bangString(key, value) {
   return typeof value == "string" ? value + "!" : value
 }
 
-return module.exports;});
+require = requireOrig;});
